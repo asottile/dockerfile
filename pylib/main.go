f3ebca9d70109bcf3d2ffd38efbc52abed8bebbf
Anthony Sottile <asottile@umich.edu>
2017-01-31 17:06:16 -0800

    Add python library wrapper around go code

---
+++ pylib/main.go
@@ -0,0 +1,174 @@
+package main
+
+// #include <stdlib.h>
+// #include <Python.h>
+//
+// int PyDockerfile_PyArg_ParseTuple_U(PyObject*, PyObject**);
+// PyObject* PyDockerfile_Py_None;
+//
+// PyObject* PyDockerfile_GoIOError;
+// PyObject* PyDockerfile_GoParseError;
+// PyObject* PyDockerfile_NewCommand(
+//     PyObject*, PyObject*, PyObject*, PyObject*, PyObject*, PyObject*
+// );
+import "C"
+import (
+	"bytes"
+	"fmt"
+	"unsafe"
+
+	"github.com/asottile/dockerfile"
+)
+
+func raise(err error) {
+	var tp *C.PyObject
+	switch err.(type) {
+	case dockerfile.IOError:
+		tp = C.PyDockerfile_GoIOError
+	case dockerfile.ParseError:
+		tp = C.PyDockerfile_GoParseError
+	default:
+		panic(err)
+	}
+	cstr := C.CString(err.Error())
+	C.PyErr_SetString(tp, cstr)
+	C.free(unsafe.Pointer(cstr))
+}
+
+func stringToPy(s string) *C.PyObject {
+	cstr := C.CString(s)
+	pystr := C.PyUnicode_FromString(cstr)
+	C.free(unsafe.Pointer(cstr))
+	return pystr
+}
+
+func stringToPyOrNone(s string) *C.PyObject {
+	if s == "" {
+		C.Py_IncRef(C.PyDockerfile_Py_None)
+		return C.PyDockerfile_Py_None
+	} else {
+		return stringToPy(s)
+	}
+}
+
+func sliceToTuple(strs []string) *C.PyObject {
+	ret := C.PyTuple_New(C.Py_ssize_t(len(strs)))
+	for i, str := range strs {
+		pystr := stringToPy(str)
+		if pystr == nil {
+			C.Py_DecRef(ret)
+			return nil
+		}
+		C.PyTuple_SetItem(ret, C.Py_ssize_t(i), pystr)
+	}
+	return ret
+}
+
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func cmdsToPy(cmds []dockerfile.Command) *C.PyObject {
+	var pyCmd, pySubCmd, pyJson, pyOriginal, pyStartLine, pyValue *C.PyObject
+	var ret *C.PyObject
+	decrefAll := func() {
+		C.Py_DecRef(pyCmd)
+		C.Py_DecRef(pySubCmd)
+		C.Py_DecRef(pyJson)
+		C.Py_DecRef(pyOriginal)
+		C.Py_DecRef(pyStartLine)
+		C.Py_DecRef(pyValue)
+		C.Py_DecRef(ret)
+	}
+
+	ret = C.PyTuple_New(C.Py_ssize_t(len(cmds)))
+	for i, cmd := range cmds {
+		pyCmd = stringToPyOrNone(cmd.Cmd)
+		if pyCmd == nil {
+			decrefAll()
+			return nil
+		}
+
+		pySubCmd = stringToPyOrNone(cmd.SubCmd)
+		if pySubCmd == nil {
+			decrefAll()
+			return nil
+		}
+
+		pyJson = C.PyBool_FromLong(C.long(boolToInt(cmd.Json)))
+
+		pyOriginal = stringToPyOrNone(cmd.Original)
+		if pyOriginal == nil {
+			decrefAll()
+			return nil
+		}
+
+		pyStartLine = C.PyLong_FromLong(C.long(cmd.StartLine))
+
+		pyValue = sliceToTuple(cmd.Value)
+		if pyValue == nil {
+			decrefAll()
+			return nil
+		}
+
+		pyCmd := C.PyDockerfile_NewCommand(
+			pyCmd, pySubCmd, pyJson, pyOriginal, pyStartLine, pyValue,
+		)
+		if pyCmd == nil {
+			decrefAll()
+			return nil
+		}
+		C.PyTuple_SetItem(ret, C.Py_ssize_t(i), pyCmd)
+	}
+	return ret
+}
+
+func goStringFromArgs(args *C.PyObject) (string, error) {
+	var obj *C.PyObject
+	if C.PyDockerfile_PyArg_ParseTuple_U(args, &obj) == 0 {
+		return "", fmt.Errorf("Failed to parse arguments")
+	}
+	bytes := C.PyUnicode_AsUTF8String(obj)
+	ret := C.GoString(C.PyBytes_AsString(bytes))
+	C.Py_DecRef(bytes)
+	return ret, nil
+}
+
+//export all_cmds
+func all_cmds(self *C.PyObject) *C.PyObject {
+	return sliceToTuple(dockerfile.AllCmds())
+}
+
+//export parse_file
+func parse_file(self *C.PyObject, args *C.PyObject) *C.PyObject {
+	filename, err := goStringFromArgs(args)
+	if err != nil {
+		return nil
+	}
+	cmds, err := dockerfile.ParseFile(filename)
+	if err != nil {
+		raise(err)
+		return nil
+	}
+	return cmdsToPy(cmds)
+}
+
+//export parse_string
+func parse_string(self *C.PyObject, args *C.PyObject) *C.PyObject {
+	s, err := goStringFromArgs(args)
+	if err != nil {
+		return nil
+	}
+	cmds, err := dockerfile.ParseReader(bytes.NewBufferString(s))
+	if err != nil {
+		raise(err)
+		return nil
+	}
+	return cmdsToPy(cmds)
+}
+
+func main() {}
