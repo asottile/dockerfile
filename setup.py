cdc861945b9dd6eb398c6ce0e8428bacce8b6d17
Alan Yee <alanyee@users.noreply.github.com>
2020-01-03 14:48:54 -0800

    Update setup.py
    -Use sys.exit over exit as recommended by the docs
    -Send error messages to stderr

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import os
+import sys
 
 from setuptools import Extension
 from setuptools import setup
 
 if not os.path.exists('vendor/github.com/moby/buildkit/frontend'):
-    print('moby checkout is missing!')
-    print('Run `git submodule update --init`')
-    exit(1)
+    sys.exit('moby checkout is missing!\n'
+             'Run `git submodule update --init`')
 
 setup(
     ext_modules=[Extension('dockerfile', ['pylib/main.go'])],
