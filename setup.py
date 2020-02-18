d3cb9c4b2d5c4f0bf64803f4421652613b95cecf
Anthony Sottile <asottile@umich.edu>
2020-02-18 12:35:38 -0800

    Produce abi3 wheels

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-import os
+import os.path
+import platform
+import sys
 
 from setuptools import Extension
 from setuptools import setup
@@ -8,7 +10,32 @@ if not os.path.exists('vendor/github.com/moby/buildkit/frontend'):
     print('Run `git submodule update --init`')
     exit(1)
 
+if (
+        sys.platform != 'win32' and
+        sys.version_info >= (3,) and
+        platform.python_implementation() == 'CPython'
+):
+    try:
+        import wheel.bdist_wheel
+    except ImportError:
+        cmdclass = {}
+    else:
+        class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
+            def finalize_options(self):
+                self.py_limited_api = 'cp3{}'.format(sys.version_info[1])
+                super().finalize_options()
+
+        cmdclass = {'bdist_wheel': bdist_wheel}
+else:
+    cmdclass = {}
+
 setup(
-    ext_modules=[Extension('dockerfile', ['pylib/main.go'])],
+    ext_modules=[
+        Extension(
+            'dockerfile', ['pylib/main.go'],
+            py_limited_api=True, define_macros=[('Py_LIMITED_API', None)],
+        ),
+    ],
+    cmdclass=cmdclass,
     build_golang={'root': 'github.com/asottile/dockerfile'},
 )
